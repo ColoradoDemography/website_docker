412adfb3e01c19815accc5c76b232695f31121ec
Daniel Trone <royhobbstn@yahoo.com>
2016-07-12 23:22:41 +0000

    cant believe this idea works so well

--- index.js
+++ index.js
@@ -3,15 +3,16 @@
 var express = require("express");
 var app = express();
 
+app.use(express.static('coloradodemography.github.io/_site'));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
- 
+
 var routes = require("./routes/routes.js")(app);
 
-var server = app.listen(4008, function () {
+var server = app.listen(4008, function() {
     console.log("Listening on port %s...", server.address().port);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -2,21 +2,39 @@ var exec = require('child_process').exec;
 
 var appRouter = function(app) {
 
-    app.get("/jekyll-build", function(req, res) {
-
-    var command = "cd coloradodemography.github.io && git pull && jekyll build";
-    exec(command, {}, function(error, stdout, stderr) {
-        console.log('-- jekyll: building _site folder --');
-        console.log('error: ' + error);
-        console.log('stdout: ' + stdout);
-        console.log('stderr: ' + stderr);
-        return res.send("complete");
+    app.get("/update/jekyll-build", function(req, res) {
+        var command = "cd coloradodemography.github.io && git pull && jekyll build";
+        exec(command, {}, function(error, stdout, stderr) {
+            console.log('-- jekyll: building _site folder --');
+            console.log('error: ' + error);
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            return res.send("_site folder has been built.  COMPLETE.");
+        });
     });
 
-
+    app.get("/update/co-grants_load", function(req, res) {
+        var command = "cd coloradodemography.github.io/_site && git clone https://github.com/ColoradoDemography/CO_Grants.git";
+        exec(command, {}, function(error, stdout, stderr) {
+            console.log('-- updating or adding co_grants repo --');
+            console.log('error: ' + error);
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            return res.send(stdout + "\n" + stderr + "\nDONE.");
+        });
     });
-
-
+  
+    app.get("/update/co-grants_update", function(req, res) {
+        var command = "cd coloradodemography.github.io/_site/CO_Grants && git pull";
+        exec(command, {}, function(error, stdout, stderr) {
+            console.log('-- updating or adding co_grants repo --');
+            console.log('error: ' + error);
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            return res.send(stdout + "\n" + stderr + "\nDONE.");
+        });
+    });
+  
 }
 
 module.exports = appRouter;
