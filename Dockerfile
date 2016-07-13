0b4aba64a4c418a6c6e226b6adb33cf619e93b29
Daniel Trone <royhobbstn@yahoo.com>
2016-07-13 23:55:37 +0000

    back to one repo

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
 FROM node:5
 
+ADD . .
+
 RUN apt-get update && \
     apt-get install -y ruby ruby-dev gcc git rsync make && \
-    gem install jekyll && \
-    git clone https://github.com/ColoradoDemography/jekyll-node-build.git && \
-    cd jekyll-node-build && \
+    gem install jekyll && 
     npm install && \
     git clone https://github.com/ColoradoDemography/coloradodemography.github.io.git
 
---
+++ routes/routes.js
@@ -0,0 +1,42 @@
+var exec = require('child_process').exec;
+
+var appRouter = function(app) {
+
+    app.get("/update/jekyll-build", function(req, res) {
+        var command = "cd coloradodemography.github.io && git pull && jekyll build";
+        exec(command, {}, function(error, stdout, stderr) {
+            console.log('-- jekyll: building _site folder --');
+            console.log('error: ' + error);
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            return res.send("_site folder has been built.  COMPLETE.");
+        });
+    });
+
+    app.get("/update/load_repo", function(req, res) {
+        var repo = req.query.repo;
+        var command = "cd coloradodemography.github.io/_site && git clone https://github.com/ColoradoDemography/" + repo + ".git";
+        exec(command, {}, function(error, stdout, stderr) {
+            console.log('-- updating or adding ' + repo + ' repo --');
+            console.log('error: ' + error);
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            return res.send(stdout + "\n" + stderr + "\nDONE.");
+        });
+    });
+  
+    app.get("/update/update_repo", function(req, res) {
+        var repo = req.query.repo;
+        var command = "cd coloradodemography.github.io/_site/" + repo + " && git pull";
+        exec(command, {}, function(error, stdout, stderr) {
+            console.log('-- updating or adding ' + repo + ' repo --');
+            console.log('error: ' + error);
+            console.log('stdout: ' + stdout);
+            console.log('stderr: ' + stderr);
+            return res.send(stdout + "\n" + stderr + "\nDONE.");
+        });
+    });
+  
+}
+
+module.exports = appRouter;
