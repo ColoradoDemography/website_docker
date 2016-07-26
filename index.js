fb8dd4351ff990493cd691af9f6b88da17cd28c4
Daniel Trone <royhobbstn@yahoo.com>
2016-07-26 15:06:56 +0000

    404 page

--- index.js
+++ index.js
@@ -18,6 +18,11 @@ var routes = require("./routes/routes.js")(app);
 //     res.send(404, 'Page not found');
 // });
 
+  
+app.use(function(req, res, next) {
+  res.status(404).send('Sorry cant find that!');
+});
+
 var server = app.listen(4008, function() {
     console.log("Listening on port %s...", server.address().port);
 });
